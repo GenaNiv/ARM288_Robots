c4b71adde0906710b80037af1cdf321aa882f6b1
EricMiddleton1 <ericm@iastate.edu>
2016-03-10 17:34:34 -0600

    UART Official Commit
    Meshed code for UART

--- open_interface.c
+++ open_interface.c
@@ -8,24 +8,33 @@
 
 void uart_init(void)
 {
-	SYSCTL_RCGCGPIO_R |= 0x03; //enable GPIO Port c
-	SYSCTL_RCGCUART_R = 0x04; //enable and provide clock for UART mod 3
+	//TODO: Calculate this from baudRate
+	uint16_t iBRD = 0x08;
+	uint16_t fBRD = 0x2C;
 
-	GPIO_PORTC_AFSEL_R = 0xC0; // set PC7:6 in AFSEL for UART3 Tx and Rx
-	GPIO_PORTC_PCTL_R = 0x11000000; //AFSEL has bit 6 and 7 set so we need nibble 6 and 7 set in PCTL for PB1:0
-	GPIO_PORTC_DEN_R = 0xC0; //enable PC7:6
+	SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R2; //enable GPIO Port C
 
-	UART3_CTL_R &= 0xFFFFFFFE; //disable uart 3 by clearing EN in CTL register
-	UART3_IBRD_R = 0x08; //BRD=SYSCLK/((ClkDiv)(BaudRate)), HSE=0 ClkDiv=16, BaudRate=115,200
-	UART3_FBRD_R = 0x2C; //Fractional remainder is 0.6805, DIVFRAC = (.6805)(64)+0.5 = 44
-	UART3_LCRH_R |= 0x60; //8 bit, 1 stop, no parity, FIFO's diasabled, 0x70 enables FIFO's
-	UART3_CC_R = 0x0; //set SysClk as source
-	UART3_CTL_R |= 0x01; //Enable UART3
+	SYSCTL_RCGCUART_R |= SYSCTL_RCGCUART_R3; //enable UART3
+
+	GPIO_PORTC_AFSEL_R |= (BIT6 | BIT7); //Enable alternate function on PC6,PC7
+	GPIO_PORTC_PCTL_R |= 0x11000000; //Enable function 1 (UART Rx/Tx) on PC6,PC7
+	GPIO_PORTC_DEN_R |= (BIT6 | BIT7); //Enable PC6,PC7 output
+	GPIO_PORTC_DIR_R |= BIT7; //Set pin direction to output on PC7
+	//GPIO_PORTC_DIR_R &= ~BIT7; //Set pin direction to input on PC6
+
+	UART3_CTL_R &= ~(UART_CTL_UARTEN); //Disable UART3 while we mess with it
+
+	UART3_IBRD_R = iBRD;
+	UART3_FBRD_R = fBRD;
+
+	UART3_LCRH_R = UART_LCRH_WLEN_8; //8 bit, 1 stop, no parity, no FIFO
+	UART3_CC_R = UART_CC_CS_SYSCLK; //Use System Clock
+	UART3_CTL_R = UART_CTL_RXE | UART_CTL_TXE | UART_CTL_UARTEN; //Enable Rx, Tx and UART module
 }
 
 void uart_sendChar(char data)
 {
-	while((UART3_FR_R & 0x20) != 0); //holds until no data in transmit buffer
+	while((UART3_FR_R & UART_FR_TXFF) != 0); //holds until no data in transmit buffer
 	UART3_DR_R = data; //puts data in transmission buffer
 }
 
@@ -34,8 +43,10 @@ char uart_receive(void)
 	//uint32_t tempData; //used for error checking
 	char data;
 
-	while((UART3_FR_R & 0x10) != 0); //wait here until data is recieved
-	data = UART3_DR_R;
+	while((UART3_FR_R & UART_FR_RXFE)); //wait here until data is recieved
+
+	data = (char)(UART3_DR_R & 0xFF);
+
 	//ToDo: Implement error checking
 	return data;
 }
